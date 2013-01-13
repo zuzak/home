e38dbc5d56ddb4a38a48afc94c48daeafe51aae1
Douglas Gardner <douglas@chippy.ch>
2013-01-13 19:25:54 +0000

    add analytics

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script src="isotope/jquery.isotope.min.js"></script>
 	<script src="scripts.js"></script>
+	<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-37637266-1']);
+	_gaq.push(['_setDomainName', 'douglasgardner.com']);
+	_gaq.push(['_setAllowLinker', true]);
+	_gaq.push(['_trackPageview']);
+
+    (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  	})();
+	</script>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<meta name="description" content="">
 	<meta name="author" content="Douglas Gardner">
